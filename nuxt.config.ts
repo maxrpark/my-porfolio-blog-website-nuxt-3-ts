44b08bfcc147463fe9dda3fea453296f024908d2
maxrpark <projectoindit@gmail.com>
2022-10-22 18:58:48 +0900

    gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,5 @@ export default defineNuxtConfig({
         strict: true,
     },
 
-    meta: {
-        script: [
-            {
-                src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js",
-                type: "text/javascript",
-            },
-            {
-                src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/Flip.min.js",
-                type: "text/javascript",
-            },
-        ],
-    },
-
     css: ["@/assets/styles/style.css"],
 });
