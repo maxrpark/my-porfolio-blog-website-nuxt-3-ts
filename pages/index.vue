d43765ccf00301e47dc103a6aa5fc1e8a65a6062
maxirpark <maxirutipark@gmail.com>
2023-08-07 11:59:43 +0900

    home title

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,43 @@
 <template>
-    <div>
-        <UiHero title="Maxi Ruti" desc="Welcome to my Portfolio-Blog"
-            ><UiSocialIcons />
-        </UiHero>
-        <div class="projects">
-            <UiSectionTitle section="projects" url="/projects" />
-            <ul class="project-container" v-if="projects">
-                <ProjectsSingleProject
-                    v-for="project in projects.slice(0, 6)"
-                    :project="project"
-                    :key="project.id"
-                />
-            </ul>
-        </div>
-        <div class="blog-section">
-            <UiSectionTitle section="post" url="/blog" />
-            <ul class="cards">
-                <BlogPostCard
-                    v-for="post of posts!.slice(0, 4)"
-                    :key="post.id"
-                    :post="post"
-                />
-            </ul>
-        </div>
+  <div>
+    <UiHero title="Maxi Ruti" desc="Creative Developer"
+      ><UiSocialIcons />
+    </UiHero>
+    <div class="projects">
+      <UiSectionTitle section="projects" url="/projects" />
+      <ul class="project-container" v-if="projects">
+        <ProjectsSingleProject
+          v-for="project in projects.slice(0, 6)"
+          :project="project"
+          :key="project.id"
+        />
+      </ul>
     </div>
+    <div class="blog-section">
+      <UiSectionTitle section="post" url="/blog" />
+      <ul class="cards">
+        <BlogPostCard
+          v-for="post of posts!.slice(0, 4)"
+          :key="post.id"
+          :post="post"
+        />
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Project, Post } from "../ts/interfaces";
 const { data } = await useFetch<string>(
-    () => "https://my-portfolio-blog-website.netlify.app/api/myProjects"
+  () => "https://my-portfolio-blog-website.netlify.app/api/myProjects"
 );
 
 const projects = JSON.parse(data.value!).filter(
-    (project: Project) => project.featured === true
+  (project: Project) => project.featured === true
 );
 
 const { data: posts } = await useFetch<Post[]>(
-    () => `https://dev.to/api/articles?username=maxrpark`
+  () => `https://dev.to/api/articles?username=maxrpark`
 );
 </script>
 
